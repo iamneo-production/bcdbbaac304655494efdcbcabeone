b352907f60b29f2ba03620acc0949a0a085b96e2
Iamneo <techadmin@iamneo.ai>
2023-09-03 03:27:57 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,64 +1,59 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
 ];
+const cells = document.querySelectorAll('.cell');
+const resultDisplay = document.getElementById('result');
+            function checkWinner() {
+    for (let i = 0; i < winPatterns.length; i++) {
+        const [a, b, c] = winPatterns[i];
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            gameActive = false;
+            return gameBoard[a];
+        }
+    }
+    if (!gameBoard.includes('')) {
+        gameActive = false;
+        return 'Draw';
+    }
+    return null;
+}
+function handleResult(result) {
+    if (result === 'Draw') {
+        resultDisplay.textContent = "It's a Draw!";
+    } else {
+        resultDisplay.textContent = `Player ${result} Won`;
+    }
+}
+function makeMove(cell) {
+    const index = Array.from(cells).indexOf(cell);
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        const winner = checkWinner();
+        if (winner) {
+            handleResult(winner);
+        } else {
+            resultDisplay.textContent = `Player ${currentPlayer} Turn`;
+        }
+    }
+}
+function resetGame() {
+    currentPlayer = 'X';
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+    cells.forEach(cell => cell.textContent = '');
+    resultDisplay.textContent = `Player ${currentPlayer} Turn`;
+}
+resetGame();
 
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
-
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
-
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
-
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
+cell.forEach((cell, i) => {
+    btn.addEventListener('click', () => ticTacToe(cell, i));
 });
 
 document.querySelector('#reset').addEventListener('click', resetGame);
